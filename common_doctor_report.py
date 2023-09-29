ab0816339c1d363850820300e104e5e65898433e
gargpalak123 <palak@proefico.com>
2023-09-29 11:17:44 +0530

    file management

--- common_doctor_report.py
+++ common_doctor_report.py
@@ -1,23 +1,64 @@
+import os
 import subprocess
-import webbrowser
+import uuid
+import shutil
 
 
 def run_tests():
-    # Run common tests and generate a report
-    subprocess.run(
-        ['robot', '--outputdir', 'common_report', '--output', 'common_report.xml', 'robot_tests/common_tests.robot'])
+    # Set the destination directory for final reports
+    destination_directory = 'C:\\Automation result'
 
-    # Run doctor-specific tests and generate a report
-    subprocess.run(
-        ['robot', '--outputdir', 'doctor_report', '--output', 'doctor_report.xml', 'robot_tests/doctor_tests.robot'])
+    # Set the working directory to where the script is located
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_directory)
 
-    # Merge reports
-    subprocess.run(['rebot', '--outputdir', 'merged_report', '--output', 'merged_report.html', 'common_report.xml',
-                    'doctor_report.xml'])
+    try:
+        # Generate unique output file names for common and doctor-specific reports
+        common_output_file = os.path.join('common_report', f'report_common_{str(uuid.uuid4())}.xml')
+        doctor_output_file = os.path.join('doctor_report', f'report_doctor_{str(uuid.uuid4())}.xml')
 
-    # Open the merged report in a web browser
-    report_url = 'file://' + os.path.abspath('merged_report/merged_report.html')
-    webbrowser.open(report_url)
+        # Run common tests with specific tags and exclude reception and admin keywords
+        subprocess.run([
+            'robot',
+            '--outputdir', 'common_report',
+            '--output', common_output_file,
+            '--include', 'common',
+            '--exclude', 'reception', '--exclude', 'admin',
+            'robot_tests/common_tests.robot'
+        ])
+
+        # Run doctor-specific tests and generate an XML report
+        subprocess.run([
+            'robot',
+            '--outputdir', 'doctor_report',
+            '--output', doctor_output_file,
+            '--include', 'doctor',
+            'robot_tests/doctor_tests.robot'
+        ])
+
+        # Copy common report to the destination directory
+        common_destination = os.path.join(destination_directory, os.path.basename(common_output_file))
+        shutil.copy(common_output_file, common_destination)
+
+        # Copy doctor report to the destination directory
+        doctor_destination = os.path.join(destination_directory, os.path.basename(doctor_output_file))
+        shutil.copy(doctor_output_file, doctor_destination)
+
+        # Merge reports
+        merged_report_path = os.path.join(destination_directory, 'merged_report.xml')
+        subprocess.run([
+            'rebot',
+            '--outputdir', destination_directory,
+            '--output', 'merged_report.xml',
+            common_output_file,
+            doctor_output_file
+        ])
+
+        print("Tests completed successfully. Reports saved in:", destination_directory)
+
+    except Exception as e:
+        print("Error:", str(e))
+        # You can log the error or perform additional error-handling actions here
 
 
 if __name__ == "__main__":
